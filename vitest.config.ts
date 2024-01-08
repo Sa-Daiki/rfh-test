83fe02c43fa6d2d4698cf8fdfc207c91102b86b0
Sa-Daiki <d325s353@gmail.com>
2024-01-08 18:06:27 +0900

    chore: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
   },
-})
+  resolve: {
+    // https://stackoverflow.com/questions/75798479/how-can-i-solve-the-issue-of-failed-to-resolve-import-in-vitest
+    alias: [{ find: "@", replacement: resolve(__dirname, "./") }],
+  },
+});
